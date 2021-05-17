5ed3de4edbbdf652bcf1dd5a7376539a7e36350f
Nathan <n_corghi@hotmail.com>
2021-05-17 19:36:15 -0300

    Incluido log na API

--- src/main/java/com/automation/utils/RequestUtils.java
+++ src/main/java/com/automation/utils/RequestUtils.java
@@ -2,16 +2,17 @@ package com.automation.utils;
 
 import com.automation.Constants;
 import com.automation.model.ResponseData;
-import com.automation.service.AbstractService;
 import com.google.gson.Gson;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RequestUtils extends AbstractService {
+public class RequestUtils {
 
     @Autowired
     private Gson gson;
@@ -23,6 +24,8 @@ public class RequestUtils extends AbstractService {
 
     private Response response;
 
+    private static final Logger logger = LoggerFactory.getLogger(RequestUtils.class);
+
     public <T> ResponseData<T> post(String url, Object request, Class<T> clazz) {
 
         RestAssured.baseURI = Constants.BASE_URL;
@@ -33,6 +36,11 @@ public class RequestUtils extends AbstractService {
 
         response = requestSpecification.body(gson.toJson(request))
                 .post(url);
+
+        logger.info("REQUEST -> Executing POST on {}", url);
+        logger.info("REQUEST -> Body: {}", gson.newBuilder().setPrettyPrinting().create().toJson(request));
+        logger.info("STATUS -> Code {}", response.statusCode());
+        logger.info("RESPONSE -> Body {}", gson.newBuilder().setPrettyPrinting().create().toJson(response.getBody().as(Object.class)));
 
         responseData.setData(response.then().extract().response().getBody().as(clazz));
         responseData.setStatusCode(response.then().extract().response().getStatusCode());
